4d9de3318d6c644d3e53e543de95f36e9c9fcaa5
Ardhendu <ardhendu201023@gmail.com>
2021-04-04 22:48:38 +0530

    int commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = ()=> {
+  return(
+
+    <>
+    <h1>Welcome to React Parcel Micro App!</h1>
+    <p>Hard to get more minimal .</p>
+    <small>This is ARDHENDU SHEKHAR</small>
+  </>
+  );
+  
+  };
+export default App;
