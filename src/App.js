c204f7c16f36c9df16b9239873be849f4b428cdf
Ardhendu <ardhendu201023@gmail.com>
2021-04-05 00:52:00 +0530

    created board and square components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
+import Board from './components/Board'
 
 const App = ()=> {
   return(
 
-    <>
-    <h1>Welcome to React Parcel Micro App!</h1>
-    <p>Hard to get more minimal .</p>
-    <small>This is ARDHENDU SHEKHAR</small>
-  </>
+    <div>
+    <h1>TIC TAC TOE</h1>
+    <Board />
+    
+    
+  </div>
   );
   
   };
